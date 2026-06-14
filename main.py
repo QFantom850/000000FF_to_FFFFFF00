acab1000d0883df4210e4aacdb1e919676199a90
QFantom850 <romankupcov948@gmail.com>
2026-06-14 22:41:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
+from pathlib import Path
 from PIL import Image
 
 
-img = Image.open(input("image filename: ")).convert("L")
+img_path = Path(input("image path: "))
 inverse_mode = input("use inverted mode? (y/n): ").lower() == "y"
-new_img = Image.new("RGBA", img.size)
-base_color = 0 if inverse_mode else 255
 
-for x in range(img.width):
-    for y in range(img.height):
-        
-        brightness = img.getpixel((x, y))
-        alpha = (255 - brightness) if inverse_mode else brightness
+with Image.open(img_path) as img:
+    
+    img = img.convert("L")
+    new_img = Image.new("RGBA", img.size)
+    base_color = 0 if inverse_mode else 255
 
-        new_img.putpixel((x, y), (base_color, base_color, base_color, alpha))
+    for x in range(img.width):
+        for y in range(img.height):
+    
+            brightness = img.getpixel((x, y))
+            alpha = (255 - brightness) if inverse_mode else brightness
 
-new_img.save("output.png")
-print("background removed successfully")
+            new_img.putpixel((x, y), (base_color, base_color, base_color, alpha))
+
+    new_img.save("output.png")
+    print("background removed successfully")
