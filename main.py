5f4eee8449547bc5768af0613d8b142fe4084e72
QFantom850 <romankupcov948@gmail.com>
2026-06-14 21:43:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from PIL import Image
+
+
+img = Image.open(input("image filename: ")).convert("L")
+inverse_mode = input("use inverted mode? (y/n): ").lower() == "y"
+new_img = Image.new("RGBA", img.size)
+base_color = 0 if inverse_mode else 255
+
+for x in range(img.width):
+    for y in range(img.height):
+        
+        brightness = img.getpixel((x, y))
+        alpha = (255 - brightness) if inverse_mode else brightness
+
+        new_img.putpixel((x, y), (base_color, base_color, base_color, alpha))
+
+new_img.save("output.png")
+print("background removed successfully")
